277b53c9d4654e8e82bedfad88ea5397e91fdce0
ATaoyoyo <1029187998@qq.com>
2021-01-03 12:04:01 +0800

    feat(new file): button组件编写完毕

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React from 'react'
+import Button, { ButtonSize, ButtonType } from './components/Button/button'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button autoFocus>Default</Button>
+      <Button disabled>Disabled</Button>
+      <Button btnType={ButtonType.Primary} onClick={() => alert('Primary')}>
+        Primary
+      </Button>
+      <Button btnType={ButtonType.Danger}>Danger</Button>
+      <Button btnType={ButtonType.Default}>Default</Button>
+      <Button btnType={ButtonType.Primary}>Primary</Button>
+
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>
+        Small Button
+      </Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+        Large Button
+      </Button>
+
+      <Button btnType={ButtonType.Link}>Link Button</Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
