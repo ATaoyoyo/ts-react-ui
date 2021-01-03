2c25eca275e5ad2665187bc285739bfbf43e23de
ATaoyoyo <1029187998@qq.com>
2021-01-03 15:22:41 +0800

    fix(button component): 修复Button组件padding不正常的问题

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import Button, { ButtonSize, ButtonType } from './components/Button/button'
+import Menu from './components/Menu/menu'
+import MenuItem from './components/Menu/menuItem'
 
 function App() {
   return (
     <div className="App">
+      <Menu defaultIndex={0} onSelect={(index) => alert(index)}>
+        <MenuItem index={0}>Lorem</MenuItem>
+        <MenuItem index={1} disabled>Lorem</MenuItem>
+        <MenuItem index={2}>Lorem</MenuItem>
+        <MenuItem index={3}>Lorem</MenuItem>
+      </Menu>
+
+      <Menu mode={"vertical"} defaultIndex={0} onSelect={(index) => alert(index)}>
+        <MenuItem index={0}>Lorem</MenuItem>
+        <MenuItem index={1} disabled>Lorem</MenuItem>
+        <MenuItem index={2}>Lorem</MenuItem>
+        <MenuItem index={3}>Lorem</MenuItem>
+      </Menu>
+
       <Button autoFocus>Default</Button>
       <Button disabled>Disabled</Button>
       <Button btnType={ButtonType.Primary} onClick={() => alert('Primary')}>
